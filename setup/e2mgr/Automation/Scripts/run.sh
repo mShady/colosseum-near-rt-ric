6347fd0a5aa321f3b52780fbd096c43d998cd77a
Shady M. Najib <Shady.Najib@breadfast.com>
2022-10-28 21:38:55 +0200

    workaround to run local without nexus3
    Mostly replacing nexus3.o-ran-sc.org by Docker hub

--- setup/e2mgr/Automation/Scripts/run.sh
+++ setup/e2mgr/Automation/Scripts/run.sh
@@ -16,8 +16,8 @@ docker rm -f dbass
 docker ps
 sleep 2
 
-#docker pull nexus3.o-ran-sc.org:10004/ric-plt-e2:$E2T_TAG
-#docker pull nexus3.o-ran-sc.org:10004/ric-plt-e2mgr:$E2M_TAG
+#docker pull nexus3.o-ran-sc.org/ric-plt-e2:$E2T_TAG
+#docker pull nexus3.o-ran-sc.org/ric-plt-e2mgr:$E2M_TAG
 docker pull snapshot.docker.ranco-dev-tools.eastus.cloudapp.azure.com:10001/e2:$E2T_TAG
 docker pull snapshot.docker.ranco-dev-tools.eastus.cloudapp.azure.com:10001/e2mgr:$E2M_TAG
 docker pull snapshot.docker.ranco-dev-tools.eastus.cloudapp.azure.com:10001/gnbe2_simu:$SIM_TAG
@@ -26,11 +26,11 @@ docker pull snapshot.docker.ranco-dev-tools.eastus.cloudapp.azure.com:10001/dbas
 
 docker run -d --name dbass -p 6379:6379 --env DBAAS_SERVICE_HOST=10.0.2.15  snapshot.docker.ranco-dev-tools.eastus.cloudapp.azure.com:10001/dbass:1.0.0
 
-#docker run -d --name e2mgr -p 3800:3800 -p 3801:3801 --env DBAAS_SERVICE_HOST=10.0.2.15 --env RMR_VCTL_FILE=/tmp/rmr.verbose nexus3.o-ran-sc.org:10004/ric-plt-e2mgr:$E2M_TAG
+#docker run -d --name e2mgr -p 3800:3800 -p 3801:3801 --env DBAAS_SERVICE_HOST=10.0.2.15 --env RMR_VCTL_FILE=/tmp/rmr.verbose nexus3.o-ran-sc.org/ric-plt-e2mgr:$E2M_TAG
 docker run -d --name e2mgr -p 3800:3800 -p 3801:3801 --env DBAAS_SERVICE_HOST=10.0.2.15 --env RMR_VCTL_FILE=/tmp/rmr.verbose snapshot.docker.ranco-dev-tools.eastus.cloudapp.azure.com:10001/e2mgr:$E2M_TAG
 #docker cp e2mgr:/opt/E2Manager/router.txt .
 sleep 2
-#docker create --name e2 --env sctp=5577 --env nano=38000 --env loglevel=debug --env print=1 -p 38000:38000 nexus3.o-ran-sc.org:10004/ric-plt-e2:$E2T_TAG
+#docker create --name e2 --env sctp=5577 --env nano=38000 --env loglevel=debug --env print=1 -p 38000:38000 nexus3.o-ran-sc.org/ric-plt-e2:$E2T_TAG
 docker create --name e2 --env sctp=5577 --env nano=38000 --env loglevel=debug --env print=1 -p 38000:38000 snapshot.docker.ranco-dev-tools.eastus.cloudapp.azure.com:10001/e2:$E2T_TAG
 sleep 2
 #docker cp router.txt e2:/opt/e2/dockerRouter.txt
@@ -50,7 +50,7 @@ docker ps
 fi
 if [ "$COMP" = "e2" ]; then
 docker rm -f e2
-#docker pull nexus3.o-ran-sc.org:10004/ric-plt-e2:$E2T_TAG
+#docker pull nexus3.o-ran-sc.org/ric-plt-e2:$E2T_TAG
 docker pull snapshot.docker.ranco-dev-tools.eastus.cloudapp.azure.com:10001/e2:$E2T_TAG
 docker create --name e2 --env sctp=5577 --env nano=38000 --env print=1 --env RMR_RTG_SVC=10.0.2.15 -p 38000:38000 snapshot.docker.ranco-dev-tools.eastus.cloudapp.azure.com:10001/e2:$E2T_TAG
 
@@ -58,9 +58,9 @@ docker ps
 fi
 if [ "$COMP" = "e2mgr" ]; then
 docker rm -f e2mgr
-#docker pull nexus3.o-ran-sc.org:10004/ric-plt-e2mgr:$E2M_TAG
+#docker pull nexus3.o-ran-sc.org/ric-plt-e2mgr:$E2M_TAG
 docker pull snapshot.docker.ranco-dev-tools.eastus.cloudapp.azure.com:10001/e2mgr:$E2M_TAG
-#docker run -d --name e2mgr -p 3800:3800 -p 3801:3801 --env RMR_RTG_SVC=10.0.2.15 --env DBAAS_SERVICE_HOST=10.0.2.15 nexus3.o-ran-sc.org:10004/ric-plt-e2mgr:$E2M_TAG
+#docker run -d --name e2mgr -p 3800:3800 -p 3801:3801 --env RMR_RTG_SVC=10.0.2.15 --env DBAAS_SERVICE_HOST=10.0.2.15 nexus3.o-ran-sc.org/ric-plt-e2mgr:$E2M_TAG
 docker run -d --name e2mgr -p 3800:3800 -p 3801:3801 --env RMR_RTG_SVC=10.0.2.15 --env DBAAS_SERVICE_HOST=10.0.2.15 snapshot.docker.ranco-dev-tools.eastus.cloudapp.azure.com:10001/e2mgr:$E2M_TAG
 docker ps
 fi
